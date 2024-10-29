4e83ae0b5c1a6281a1565ac358b95dc2795f4797
sara <matika20@gmail.com>
2024-10-29 20:48:12 +0100

    changing username/password fields

--- src/components/loginForm/LoginForm.jsx
+++ src/components/loginForm/LoginForm.jsx
@@ -7,12 +7,12 @@ function LoginForm(){
         <form className="login-form">
             <label htmlFor="username-field" id="username-label">
                 username:
-                <input type='text' id="username-field"  name="username" placeholder = "Username"  />
+                <input type='text' id="username-field"  name="username" placeholder = "Username"  onChange={(e) => console.log(e.target.value)}/>
                 {/*onChange={( e : ChangeEvent<HTMLInputElement>) => console.log(e.target.value)}*/}
             </label>
             <label htmlFor="password-field" id="password-label">
                 Password:
-                <input type='password' id="password-field"/>
+                <input type='password' id="password-field" name="password" placeholder = "Password"  onChange={(e) => console.log(e.target.value)}/>
             </label>
             <Button buttonName = "Log In"/>
             <p>
