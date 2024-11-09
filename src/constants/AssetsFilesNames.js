a4fb98424e7b4ceac0661a5e5b78c458f86ba0af
sara <matika20@gmail.com>
2024-11-09 01:31:29 +0100

    creating a file that is used to store and export constants that represent the paths to various asset files (such as images) used in the project

---
+++ src/constants/AssetsFilesNames.js
@@ -0,0 +1,6 @@
+
+export const SettingsIcon = "/src/assets/settings-icon-24px.png";
+export const HeaderLogoImg ="/src/assets/logo-100x100.png";
+export const CoverImage = "/src/assets/cover-image.jpg";
+export const SignatureDish = "/src/assets/signature-dish-250x250.jpeg";
+export const FooterWrapLogo = "/src/assets/logo-50x50.png";
