9dbf1d442af4da486a1fcae95d6ef11cfcb55cae
sara <matika20@gmail.com>
2024-11-09 01:29:30 +0100

    creatig a table component to fetch employees data from the backend and display received record in a table form

--- src/components/employeesDataTable/EmployeesDataTable.jsx
+++ src/components/employeesDataTable/EmployeesDataTable.jsx
@@ -1,8 +1,33 @@
 import "./EmployeesDataTable.css";
+import {useEffect, useState} from "react";
+import {deoudegrachtApi, employeesEndpoint} from "../../deoudegrachtApi.js";
+import EmployeeRecordTableRow from "../employeeRecordTableRow/EmployeeRecordTableRow.jsx";
 
 function EmployeesDataTable(){
-    return(
+    const [employees, setEmployees] = useState([]);
+    const [loading, setLoading] = useState(true);
 
+    useEffect(()=>{
+        const fetchEAllEmployees = async ()=>{
+            try {
+                const response = await deoudegrachtApi.get(employeesEndpoint);
+                console.log("Employees data fetched", response.data);
+                setEmployees(response.data);
+                setLoading(false);
+            }
+            catch (e){
+                console.log("Error fetching employees", e.data);
+                setLoading(false);
+            }
+        }
+        fetchEAllEmployees();
+    },[]);
+    useEffect(() => {
+        console.log("Employees data set", employees);
+        setLoading(false);
+    }, [employees]);
+
+    return(
         <div className="employees-data-table">
             <table>
                 <thead>
@@ -10,16 +35,26 @@ function EmployeesDataTable(){
                         <th>First Name</th>
                         <th>Last Name</th>
                         <th>Email</th>
-                        <th>Mobile Number</th>
+                        <th>Phone Number</th>
                         <th>Username</th>
                         <th>Role</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {/*<tr>*/}
-                    {/*    <td>John</td>*/}
-                    {/*    <td>Doe</td>*/}
-                    {/*</tr>*/}
+                {
+                    loading ? (
+                        <tr>
+                            <td colSpan="6">Loading...</td>
+                        </tr>
+                    ):(
+                        employees.map(
+                            (employee, index) => (
+                                console.log("Employee data", employee),
+                                <EmployeeRecordTableRow key={index} employee={employee} />
+                            )
+                        )
+                    )
+                }
                 </tbody>
             </table>
 
