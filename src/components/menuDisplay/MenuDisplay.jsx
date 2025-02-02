a592be198a1946722665cbfb06cbddef9cfd3d3b
sara <matika20@gmail.com>
2025-02-02 02:02:26 +0100

    commit

--- src/components/menuDisplay/MenuDisplay.jsx
+++
@@ -1,9 +0,0 @@
-import "./MenuDisplay.css";
-function MenuDisplay(){
-    return(
-        <div className="menu-display">
-            <h1>De Oudegracht Menu</h1>
-        </div>
-    );
-}
-export default MenuDisplay;
