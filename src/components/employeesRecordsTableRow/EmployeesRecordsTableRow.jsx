9349626e3eb2d8e6dbacd79399ecd32f29d4d956
Sara Elbana <150337030+saraelbana@users.noreply.github.com>
2025-02-14 11:29:42 +0100

    implement delete button  in employee table row file

--- src/components/employeesRecordsTableRow/EmployeesRecordsTableRow.jsx
+++ src/components/employeesRecordsTableRow/EmployeesRecordsTableRow.jsx
@@ -2,19 +2,41 @@ import "./EmployeesRecordsTableRow.css";
 import PropTypes from 'prop-types';
 import { useNavigate } from "react-router-dom";
 import Button from "../button/Button.jsx";
-import { EDIT_ICON } from "../../constants/AssetsFilesNames.js";
-import {useContext} from "react";
+import {DELETE_ICON, EDIT_ICON} from "../../constants/AssetsFilesNames.js";
+import {useContext, useState} from "react";
 import {AuthContext} from "../../context/authContext/AuthContext.jsx";
+import {deleteEmployee} from "../../helpers/APIOperations.js";
 
 function EmployeesRecordsTableRow({ employee }) {
     const {user} = useContext(AuthContext);
     const navigate = useNavigate();
+    const [error, setError] = useState(null);
+    const [success, setSuccess] = useState(null);
     const handleUsernameClick = () => {
         navigate(`/portal/employee/${employee.username}`);
     };
     const handleEditClick = () => {
         navigate(`/portal/employee/${employee.username}?edit=true`);
     };
+    const handleDeleteClick = async () => {
+        if (user.role === "ADMIN" && employee.role !== "ADMIN") {
+            try {
+                console.log(employee.username);
+                const response = await deleteEmployee(employee.username);
+                if (response[0] === 1) {
+                    setSuccess("Employee deleted successfully");
+                    setError("");
+                    setTimeout(() => {
+                        navigate("/portal/employee");
+                    }, 5000);
+                }
+            } catch (error) {
+                console.error(`Failed to delete employee with username: ${employee.username}`);
+                setError("Failed to delete employee");
+                setSuccess("");
+            }
+        }
+    }
 
     return (
         <tr>
@@ -32,18 +54,34 @@ function EmployeesRecordsTableRow({ employee }) {
             {
                 (user.role === "ADMIN") &&
                 (
-                    <td>
-                        <div className="table-action-buttons">
-                            <Button
-                                size="icon"
-                                iconSrc={EDIT_ICON}
-                                onClick={handleEditClick}
-                                className="edit-button"
-                            />
-                        </div>
-                    </td>
+                    <div className="table-action-buttons">
+                        <td>
+                            <div className="table-action-buttons">
+                                <Button
+                                    size="icon"
+                                    iconSrc={EDIT_ICON}
+                                    onClick={handleEditClick}
+                                    className="edit-button"
+                                />
+                            </div>
+                        </td>
+                        {   (employee.role !== "ADMIN") &&
+                            <td>
+                                <div className="table-action-buttons">
+                                    <Button
+                                        size="icon"
+                                        iconSrc={DELETE_ICON}
+                                        onClick={handleDeleteClick}
+                                        className="delete-button"
+                                    />
+                                </div>
+                            </td>
+                        }
+                    </div>
                 )
             }
+            {error && <td className="error-message">{error}</td>}
+            {success && <td className="success-message">{success}</td>}
         </tr>
     );
 }
